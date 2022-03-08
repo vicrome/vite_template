d5917b9b268c89dfaafcb81ff8b2a430decdbd20
starfuksv <vicro96@gmail.com>
2022-03-08 22:20:25 +0100

    update theme

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,15 @@
 </script>
 
 <style>
+html {
+  background-color: #2c3e50;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: whitesmoke;
   margin-top: 60px;
 }
 </style>
